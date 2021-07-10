27a848c68f704e6aab8fc4d7119a80f47549755b
wnsdnn <a24369029@gmail.com>
2021-07-10 14:25:23 +0900

    wnsdnn

--- js/script2.js
+++ js/script2.js
@@ -14,8 +14,7 @@ const len = 300;
 let range = rangeBtn.value/10;
 let speed = speedBtn.value/5;
 
-let sumRadius = 0;
-let radius = radiusBtn.value;
+let radius = Number(radiusBtn.value);
 
 
 rangeBtn.addEventListener("input", function(){
@@ -25,7 +24,7 @@ speedBtn.addEventListener("input", function(){
     speed = speedBtn.value/5;
 });
 radiusBtn.addEventListener("input", function(){
-    radius = radiusBtn.value;
+    radius = Number(radiusBtn.value);
 });
 
 
@@ -34,15 +33,17 @@ const createSnow = function() {
     const snows = []
     for(let i = 0; i<len; i++){
         const random = Math.round(Math.random());
+        const rad = Math.random()*3;
         const snow = {
             x: window.innerWidth/100*Math.round(Math.random()*100),
             y: window.innerHeight/100*Math.round(Math.random()*100),
-            radius: Math.random()*3,
+            radius: rad,
             speed: Math.random()*10,
             opacity: Math.random(),
             direct: random > 0 ?  'right' : 'left',
             count: 0,
-            range: Math.round(Math.random()*100)+20
+            range: Math.round(Math.random()*100)+20,
+            subRadius: rad
         }
         snows.push(snow);
     }
@@ -54,13 +55,14 @@ const snows = createSnow();
 const drawSnow = function(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    
     for(let i = 0; i < snows.length; i++){
         const snow = snows[i];
 
         ctx.beginPath();
         ctx.fillStyle = `rgba(225, 225, 225, ${snow.opacity})`;
 
-        ctx.arc(snow.x, snow.y, sumRadius , 0, Math.PI*2)
+        ctx.arc(snow.x, snow.y, snow.radius, 0, Math.PI*2)
         ctx.fill();
 
     }
@@ -68,48 +70,55 @@ const drawSnow = function(){
 }
 
 const moveSnow = function() {
-    // console.log(radius);
 
     for(let i = 0; i < snows.length; i++){
         const snow = snows[i];
 
-        if(snow.direct === 'right') {
-            snow.x += range;
-            snow.count += 0.5;
-
-            if(snow.count === snow.range) {
-                snow.direct = "left";
-            };
-            
-        }else if(snow.direct === 'left') {
-            snow.x -= range;
-            snow.count -= 0.5;
-            
-            if(snow.count === snow.range*-1) {
-                snow.direct = "right";
-            }
-
-        }
-
-
         snow.y += snow.speed + speed;
 
-        sumRadius = snow.radius + radius;
-        // console.log(radius);
+        if(snow.y + snow.radius >= window.innerHeight){
+            snow.y = window.innerHeight - snow.radius;
+            snow.radius -= .8;
 
-        if(snow.y > window.innerHeight+snow.radius*2){
-            snow.y = -snow.radius*2;
+
+            if(snow.radius < 0){
+                snow.y = -snow.radius*2;
+                snow.radius = snow.subRadius + radius
+            }
+
+            
+        } else {
+            if(snow.direct === 'right') {
+                snow.x += range;
+                snow.count += 0.5;
+    
+                if(snow.count === snow.range) {
+                    snow.direct = "left";
+                };
+                
+            }else if(snow.direct === 'left') {
+                snow.x -= range;
+                snow.count -= 0.5;
+                
+                if(snow.count === snow.range*-1) {
+                    snow.direct = "right";
+                }
+    
+            }
+
+            snow.radius = snow.subRadius + radius
         }
 
+
+
     }
+
 
 
     drawSnow();
     requestAnimationFrame(moveSnow);
 };
 moveSnow();
-
-
 
 window.addEventListener("keydown", function({key}){
     if(key === "r"){
